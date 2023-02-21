30062f6f3362325aacba33cf5ad0462c837a835e
npenigaud <125455992+npenigaud@users.noreply.github.com>
2023-02-21 12:16:03 +0100

    Update spcm.sh

--- spcm.sh
+++ spcm.sh
@@ -3,6 +3,7 @@
 #SBATCH --time 00:05:00
 #SBATCH --exclusive
 #SBATCH --export="NONE"
+#SBATCH --partition ndl
 
 set -x
 
@@ -15,7 +16,7 @@ cd $SLURM_SUBMIT_DIR
 
 export SLURM_EXPORT_ENV=ALL
 export MPIAUTOCONFIG=mpiauto.PGI.conf
-#xport MPIAUTOCONFIG=mpiauto.DDT.conf
+#export MPIAUTOCONFIG=mpiauto.DDT.conf
 
  /opt/softs/mpiauto/mpiauto --nouse-slurm-mpi --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0031l015-008mpi --write-grib-1 --write-grib-2 --stat-gp
 #/opt/softs/mpiauto/mpiauto --nouse-slurm-mpi --verbose -np 8 --wrap --wrap-stdeo -- ./spcm.x --case t0107l070-008mpi --stat-gp
